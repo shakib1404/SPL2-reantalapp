a7964af397620d7af2582d5542b25109ed61d1db
shakib1404 <bsse1404@iit.du.ac.bd>
2025-02-25 14:28:01 +0600

    some

--- server/routes/message.js
+++ server/routes/message.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Message = require("../models/Message");
+const Message = require("../Models/Message");
 
 class MessageController {
     constructor() {
