27ee5742e85f3a01b46bb1f1d2824eb6789ce933
shakib1404 <bsse1404@iit.du.ac.bd>
2025-03-03 17:31:01 +0600

    home recommadition

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -154,6 +154,7 @@ const Navbar = () => {
                 <Link to={`/${user._id}/wishList`}>Wish List</Link>
                 <Link to={`/${user._id}/properties`}>Property List</Link>
                 <Link to={`/${user._id}/reservations`}>Reservation List</Link>
+                <Link to={`/rent-predictor`}>Predict The Rent</Link>
                 <Link to="/create-listing">Become A Host</Link>
                 <Link
                   to="/login"
